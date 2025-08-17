38e1adc4d6cc6a2392e83bb50bc45fe168d746d2
DawnBreaker <tunganhngo207@gmail.com>
2025-08-17 17:29:17 +0700

    feat: update movie feature

--- backend/src/main/java/com/example/backend/model/Movie.java
+++ backend/src/main/java/com/example/backend/model/Movie.java
@@ -1,9 +1,14 @@
 package com.example.backend.model;
 
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
+@Hidden
 public class Movie {
     private Long id;
     private String title;
@@ -20,124 +25,124 @@ public class Movie {
     }
 
     public Movie(Long id, String title, String poster, String overview, int duration, List<String> genre,
-	    Date releaseDate, String imdbId, String filmId) {
-	this.id = id;
-	this.title = title;
-	this.poster = poster;
-	this.overview =overview;
-	this.duration =duration;
-	this.genre = genre;
-	this.releaseDate = releaseDate;
-	this.imdbId = imdbId;
-	this.filmId = filmId;
+                 Date releaseDate, String imdbId, String filmId) {
+        this.id = id;
+        this.title = title;
+        this.poster = poster;
+        this.overview = overview;
+        this.duration = duration;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+        this.imdbId = imdbId;
+        this.filmId = filmId;
     }
 
     public Long getId() {
-	return id;
+        return id;
     }
 
     public void setId(Long id) {
-	this.id = id;
+        this.id = id;
     }
 
     public String getTitle() {
-	return title;
+        return title;
     }
 
     public void setTitle(String title) {
-	this.title = title;
+        this.title = title;
     }
 
     public String getPoster() {
-	return poster;
+        return poster;
     }
 
     public void setPoster(String poster) {
-	this.poster = poster;
+        this.poster = poster;
     }
 
     public String getOverview() {
-	return overview;
+        return overview;
     }
 
     public void setOverview(String overview) {
-	this.overview = overview;
+        this.overview = overview;
     }
 
     public int getDuration() {
-	return duration;
+        return duration;
     }
 
     public void setDuration(int duration) {
-	this.duration = duration;
+        this.duration = duration;
     }
 
     public List<String> getGenre() {
-	return genre;
+        return genre;
     }
 
     public void setGenre(List<String> genre) {
-	this.genre = genre;
+        this.genre = genre;
     }
 
     public Date getReleaseDate() {
-	return releaseDate;
+        return releaseDate;
     }
 
     public void setReleaseDate(Date releaseDate) {
-	this.releaseDate = releaseDate;
+        this.releaseDate = releaseDate;
     }
 
     public String getImdbId() {
-	return imdbId;
+        return imdbId;
     }
 
     public void setImdbId(String imdbId) {
-	this.imdbId = imdbId;
+        this.imdbId = imdbId;
     }
 
     public String getFilmId() {
-	return filmId;
+        return filmId;
     }
 
     public void setFilmId(String filmId) {
-	this.filmId = filmId;
+        this.filmId = filmId;
     }
 
     @Override
     public String toString() {
 
-	return "Movie{" + "id=" + id + 
-		", title=" + title + 
-		", poster='" + poster + '\'' + 
-		", overview='" + overview + '\'' + 
-		", duration='" + duration + '\'' + 
-		", genre='" + genre + '\'' + 
-		", releaseDate='" + releaseDate + '\'' + 
-		", imdbId='" + imdbId + '\'' + '}' + 
-		", filmId='" + filmId + '\'' + '}';
+        return "Movie{" + "id=" + id +
+                ", title=" + title +
+                ", poster='" + poster + '\'' +
+                ", overview='" + overview + '\'' +
+                ", duration='" + duration + '\'' +
+                ", genre='" + genre + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", imdbId='" + imdbId + '\'' + '}' +
+                ", filmId='" + filmId + '\'' + '}';
     }
 
     @Override
     public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null || getClass() != obj.getClass())
-	    return false;
-	Movie movie = (Movie) obj;
-	return Objects.equals(id, movie.id) 
-		&& Objects.equals(poster, movie.poster)
-		&& Objects.equals(overview, movie.overview) 
-		&& Objects.equals(duration, movie.duration)
-		&& Objects.equals(genre, movie.genre) 
-		&& Objects.equals(releaseDate, movie.releaseDate)
-		&& Objects.equals(filmId, movie.filmId) 
-		&& Objects.equals(imdbId, movie.imdbId);
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Movie movie = (Movie) obj;
+        return Objects.equals(id, movie.id)
+                && Objects.equals(poster, movie.poster)
+                && Objects.equals(overview, movie.overview)
+                && Objects.equals(duration, movie.duration)
+                && Objects.equals(genre, movie.genre)
+                && Objects.equals(releaseDate, movie.releaseDate)
+                && Objects.equals(filmId, movie.filmId)
+                && Objects.equals(imdbId, movie.imdbId);
 
     }
 
     @Override
     public int hashCode() {
-	return Objects.hash(id, poster, overview, duration, genre, releaseDate, imdbId, filmId);
+        return Objects.hash(id, poster, overview, duration, genre, releaseDate, imdbId, filmId);
     }
 }
--- backend/src/main/java/com/example/backend/service/Impl/MovieServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/Impl/MovieServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.example.backend.dto.MovieDTO;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -28,77 +29,99 @@ public class MovieServiceImpl implements MovieService {
     private final MovieRepositoryImpl movieRepository;
 
     public MovieServiceImpl(RestTemplate restTemplate, MovieRepositoryImpl movieRepository) {
-	this.restTemplate = restTemplate;
-	this.movieRepository = movieRepository;
+        this.restTemplate = restTemplate;
+        this.movieRepository = movieRepository;
     }
 
-    @Cacheable(value = MOVIE_CACHE, key = "#movie")
+//    @Cacheable(MOVIE_CACHE)
     @Override
-    public List<Movie> findAll() {
-	return movieRepository.findAll();
+    public List<Movie> findAll(MovieDTO m) {
+        return movieRepository.findAll(m);
     }
 
     @Override
-    @Cacheable(value = MOVIE_CACHE, key = "#movie")
+//    @Cacheable(MOVIE_CACHE)
     public Movie findOne(Long id) {
-	return movieRepository.findOne(id)
-		.orElseThrow(() -> new MovieNotFoundException("Not match found with id " + id));
+        return movieRepository.findOne(id)
+                .orElseThrow(() -> new MovieNotFoundException("Not match found with id " + id));
     }
 
     @Override
-    @Cacheable(value = MOVIE_CACHE, key = "#movie")
+//    @Cacheable(MOVIE_CACHE)
     public Movie findByMovieId(String id) {
-	return movieRepository.findByMovieId(id)
-		.orElseThrow(() -> new MovieNotFoundException("Not match found with id " + id));
+        return movieRepository.findByMovieId(id)
+                .orElseThrow(() -> new MovieNotFoundException("Not match found with id " + id));
     }
 
     @Override
-    public Movie create(Long id) {
-	String apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjgxYTVkNmVhYTVmZWViZjEzNWM5MTJjNzQ1YmI0MSIsIm5iZiI6MTc1MzcwMTQ4OC4zNzksInN1YiI6IjY4ODc1YzcwZTA4OGQ1NzhjNzhhNzRhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zCqZxPPYPUxf-b_MJPIyb4tgaN6F_TM_n3Jn9nK8pM8";
-	String url = "https://api.themoviedb.org/3/movie/" + id + "?language=vi-VN";
+    public Movie createWithId(Long id) {
+        String apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjgxYTVkNmVhYTVmZWViZjEzNWM5MTJjNzQ1YmI0MSIsIm5iZiI6MTc1MzcwMTQ4OC4zNzksInN1YiI6IjY4ODc1YzcwZTA4OGQ1NzhjNzhhNzRhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zCqZxPPYPUxf-b_MJPIyb4tgaN6F_TM_n3Jn9nK8pM8";
+        String url = "https://api.themoviedb.org/3/movie/" + id + "?language=vi-VN";
 
-	var checkExist = movieRepository.findByMovieId(String.valueOf(id));
+        var checkExist = movieRepository.findByMovieId(String.valueOf(id));
 
-	if (checkExist.isPresent()) {
-	    throw new MovieExistedException("This movie is existed");
+        if (checkExist.isPresent()) {
+            throw new MovieExistedException("This movie is existed");
 
-	}
+        }
 
-	HttpHeaders headers = new HttpHeaders();
-	headers.set("Authorization", "Bearer " + apiKey);
-	HttpEntity<String> entity = new HttpEntity<>(headers);
-	ResponseEntity<JsonNode> res = restTemplate.exchange(url, HttpMethod.GET, entity, JsonNode.class);
-	JsonNode root = res.getBody();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + apiKey);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<JsonNode> res = restTemplate.exchange(url, HttpMethod.GET, entity, JsonNode.class);
+        JsonNode root = res.getBody();
 
-	Movie movie = new Movie();
-	movie.setTitle(root.path("title").asText());
-	movie.setDuration(root.path("runtime").asInt());
-	movie.setOverview(root.path("overview").asText());
-	movie.setImdbId(root.path("imdb_id").asText());
-	movie.setFilmId(root.path("id").asText());
-	movie.setPoster("https://image.tmdb.org/t/p/original" + root.path("poster_path").asText());
-	movie.setReleaseDate(Date.from(
-		LocalDate.parse(root.path("release_date").asText()).atStartOfDay(ZoneId.systemDefault()).toInstant()));
+        Movie movie = new Movie();
+        movie.setTitle(root.path("title").asText());
+        movie.setDuration(root.path("runtime").asInt());
+        movie.setOverview(root.path("overview").asText());
+        movie.setImdbId(root.path("imdb_id").asText());
+        movie.setFilmId(root.path("id").asText());
+        movie.setPoster("https://image.tmdb.org/t/p/original" + root.path("poster_path").asText());
+        movie.setReleaseDate(Date.from(
+                LocalDate.parse(root.path("release_date").asText()).atStartOfDay(ZoneId.systemDefault()).toInstant()));
 
-	List<String> genres = new ArrayList<String>();
-	for (JsonNode genreNode : root.path("genres")) {
-	    genres.add(genreNode.path("name").asText());
-	}
-	movie.setGenre(genres);
-	movieRepository.save(movie);
-	return movie;
+        List<String> genres = new ArrayList<String>();
+        for (JsonNode genreNode : root.path("genres")) {
+            genres.add(genreNode.path("name").asText());
+        }
+        movie.setGenre(genres);
+        movieRepository.save(movie);
+        return movie;
+    }
+
+
+    @Override
+    public Movie create(MovieDTO m) {
+
+        Movie movie = new Movie();
+        movie.setTitle(m.getTitle());
+        movie.setDuration(m.getDuration());
+        movie.setOverview(m.getOverview());
+        movie.setImdbId(m.getImdbId());
+        movie.setFilmId(m.getFilmId());
+        movie.setPoster(m.getPoster());
+        movie.setReleaseDate(m.getReleaseDate());
+
+        List<String> genres = new ArrayList<String>();
+        for (String genreNode : m.getGenre()) {
+            genres.add(genreNode);
+        }
+        movie.setGenre(genres);
+        movieRepository.save(movie);
+        return movie;
     }
 
     @Override
     public Movie update(Long id, Movie m) {
-	movieRepository.findOne(id).orElseThrow(() -> new MovieNotFoundException("Not match foundd with id " + id));
+        movieRepository.findOne(id).orElseThrow(() -> new MovieNotFoundException("Not match foundd with id " + id));
 
-	return movieRepository.update(m);
+        return movieRepository.update(m);
     }
 
     @Override
     public void delete(Long id) {
-	movieRepository.delete(id);
+        movieRepository.delete(id);
 
     }
 
