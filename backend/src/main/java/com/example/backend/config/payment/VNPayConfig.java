5a286a56935c8a94838bd0857c6b19b89bb14610
DawnBreaker <tunganhngo207@gmail.com>
2025-08-17 17:30:49 +0700

    fix: update payment error

--- backend/src/main/java/com/example/backend/config/payment/VNPayConfig.java
+++ backend/src/main/java/com/example/backend/config/payment/VNPayConfig.java
@@ -36,33 +36,34 @@ public class VNPayConfig {
     private String orderType;
 
     public String getVnp_PayUrl() {
-	return vnp_PayUrl;
+        return vnp_PayUrl;
     }
 
     public String getVnp_secretKey() {
-	return secretKey;
+        return secretKey;
     }
 
     public Map<String, String> getVNPayConfig() {
-	Map<String, String> vnpParamsMap = new HashMap<>();
-	vnpParamsMap.put("vnp_Version", this.vnp_Version);
-	vnpParamsMap.put("vnp_Command", this.vnp_Command);
-	vnpParamsMap.put("vnp_TmnCode", this.vnp_TmnCode);
-	vnpParamsMap.put("vnp_CurrCode", "VND");
-	vnpParamsMap.put("vnp_TxnRef", VNPayUtils.getRandomNumber(8));
-	vnpParamsMap.put("vnp_OrderInfo", "Thanh toan don hang:" + VNPayUtils.getRandomNumber(8));
-	vnpParamsMap.put("vnp_OrderType", this.orderType);
-	vnpParamsMap.put("vnp_Locale", "vn");
-	vnpParamsMap.put("vnp_ReturnUrl", this.vnp_ReturnUrl);
-	Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
-	SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-	String vnpCreateDate = formatter.format(calendar.getTime());
-	vnpParamsMap.put("vnp_CreateDate", vnpCreateDate);
-	calendar.add(Calendar.MINUTE, 15);
-	String vnp_ExpireDate = formatter.format(calendar.getTime());
-	vnpParamsMap.put("vnp_ExpireDate", vnp_ExpireDate);
+        Map<String, String> vnpParamsMap = new HashMap<>();
+        vnpParamsMap.put("vnp_Version", this.vnp_Version);
+        vnpParamsMap.put("vnp_Command", this.vnp_Command);
+        vnpParamsMap.put("vnp_TmnCode", this.vnp_TmnCode);
+        vnpParamsMap.put("vnp_CurrCode", "VND");
+        vnpParamsMap.put("vnp_TxnRef", VNPayUtils.getRandomNumber(8));
+        vnpParamsMap.put("vnp_OrderInfo", "Thanh toan don hang:" + VNPayUtils.getRandomNumber(8));
+        vnpParamsMap.put("vnp_OrderType", this.orderType);
+        vnpParamsMap.put("vnp_Locale", "vn");
+        vnpParamsMap.put("vnp_ReturnUrl", this.vnp_ReturnUrl);
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        formatter.setTimeZone(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        String vnpCreateDate = formatter.format(calendar.getTime());
+        vnpParamsMap.put("vnp_CreateDate", vnpCreateDate);
+        calendar.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(calendar.getTime());
+        vnpParamsMap.put("vnp_ExpireDate", vnp_ExpireDate);
 
-	return vnpParamsMap;
+        return vnpParamsMap;
     }
 
 }
