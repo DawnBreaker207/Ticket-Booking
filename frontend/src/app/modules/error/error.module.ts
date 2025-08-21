8a93213090259e34ed59f5187e38501962c28332
DawnBreaker <tunganhngo207@gmail.com>
2025-08-21 13:29:24 +0700

    fix: update error modules

--- frontend/src/app/modules/error/error.module.ts
+++ frontend/src/app/modules/error/error.module.ts
@@ -3,13 +3,15 @@ import {CommonModule} from '@angular/common';
 
 import {ErrorRoutingModule} from './error-routing.module';
 import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {AngularSvgIconModule} from 'angular-svg-icon';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ErrorRoutingModule
+    ErrorRoutingModule,
+    AngularSvgIconModule.forRoot()
   ],
   providers: [provideHttpClient(withInterceptorsFromDi())]
 })
