2c025e252ce8acd86389db1c40b559b0f074c0fd
DawnBreaker <tunganhngo207@gmail.com>
2025-08-21 13:29:24 +0700

    fix: update error modules

--- frontend/src/app/modules/error/pages/500/error-500.component.ts
+++ frontend/src/app/modules/error/pages/500/error-500.component.ts
@@ -1,9 +1,17 @@
 import {Component, inject} from '@angular/core';
 import {Router} from '@angular/router';
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzResultComponent, NzResultExtraDirective} from "ng-zorro-antd/result";
+import {SvgIconComponent} from "angular-svg-icon";
 
 @Component({
   selector: 'app-error-500',
-  imports: [],
+    imports: [
+        NzButtonComponent,
+        NzResultComponent,
+        NzResultExtraDirective,
+        SvgIconComponent
+    ],
   templateUrl: './error-500.component.html',
   styleUrl: './error-500.component.css'
 })
