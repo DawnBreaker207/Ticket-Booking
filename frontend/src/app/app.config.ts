86b9f45fbd54e05f0c187ed6265d63c01b4c03ec
DawnBreaker <tunganhngo207@gmail.com>
2025-08-22 16:34:02 +0700

    feat: update config global

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -13,7 +13,7 @@ import {provideNzIcons} from 'ng-zorro-antd/icon';
 import {en_US, provideNzI18n} from 'ng-zorro-antd/i18n';
 import {registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideEffects} from '@ngrx/effects';
@@ -31,7 +31,7 @@ export const appConfig: ApplicationConfig = {
     provideStore(),
     provideNzIcons(icons),
     provideNzI18n(en_US),
-    importProvidersFrom(FormsModule),
+    importProvidersFrom(FormsModule, ReactiveFormsModule),
     provideAnimationsAsync(),
     provideEffects()
   ]
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import {Routes} from "@angular/router";
+import {AdminGuard} from '@/app/core/guards/admin.guard';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)},
-  {path: 'admin', loadChildren: () => import('./modules/admin/admin-module').then(m => m.AdminModule)},
+  {
+    path: 'admin',
+    canActivateChild: [AdminGuard],
+    loadChildren: () => import('./modules/admin/admin-module').then(m => m.AdminModule)
+  },
   {path: 'errors', loadChildren: () => import('./modules/error/error.module').then(m => m.ErrorModule)},
   {path: '**', redirectTo: 'errors/404'}
 ];
