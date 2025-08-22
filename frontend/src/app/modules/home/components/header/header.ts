58e42d99ba1ad5bc6d4e09ac43d5854c3ea151c2
DawnBreaker <tunganhngo207@gmail.com>
2025-08-22 16:33:49 +0700

    feat: update home module

--- frontend/src/app/modules/home/components/header/header.ts
+++ frontend/src/app/modules/home/components/header/header.ts
@@ -1,14 +1,26 @@
 import {Component} from '@angular/core';
 import {UpperCasePipe} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
-    UpperCasePipe
+    UpperCasePipe,
+    RouterLink
   ],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class HeaderComponent {
-  headers: string[] = ["Lịch chiếu rạp", "Phim", "Rạp", "Giá vé", "Tin mới và ưu đãi", "Nhượng quyền", "Thành viên"];
+  headers = [
+    {name: "Lịch chiếu rạp", path: ""},
+    {name: "Phim", path: ""},
+    {name: " Rạp", path: ""},
+    {name: "Giá vé", path: ""},
+    {name: "Tin mới và ưu đãi", path: ""},
+    {name: "Nhượng quyền", path: ""},
+    {name: "Thành viên", path: "/home/login"}
+
+
+  ];
 }
