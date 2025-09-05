37744cfbc03bf64174a88034e02f6ab2395db14c
DawnBreaker <tunganhngo207@gmail.com>
2025-09-05 16:17:20 +0700

    feat: update service

--- frontend/src/app/core/services/order/order.service.ts
+++ frontend/src/app/core/services/order/order.service.ts
@@ -1,7 +1,10 @@
 import {inject, Injectable} from '@angular/core';
 import {environment} from '@/environments/environment';
 import {HttpClient} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import {catchError, map, Observable, of} from 'rxjs';
+import {ApiRes} from '@/app/core/models/common.model';
+import {Order} from '@/app/core/models/order.model';
+import {CinemaHall} from '@/app/core/models/cinemaHall.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +13,40 @@ export class OrderService {
   URL = `${environment.apiUrl}/order`;
   private http = inject(HttpClient);
 
+  getOrders(order: Partial<Order>) {
+    return this.http.get<ApiRes<Order[]>>(`${this.URL}`, {}).pipe(
+      map((res: any) => res.data),
+      catchError(this.handleError<Order[]>('Get orders'))
+    )
+  }
+
+  getOrder(id: number) {
+    return this.http.get<ApiRes<Order>>(`${this.URL}/${id}`, {}).pipe(
+      map((res: any) => res.data),
+      catchError(this.handleError<Order>('Get order'))
+    )
+  }
+
+  initOrder(order: Partial<Order>) {
+    return this.http.post<ApiRes<string>>(`${this.URL}/init`, order).pipe(
+      map((res: any) => res.data),
+      catchError(this.handleError<string>('Init order'))
+    )
+  }
+
+  holdSeat(order: Partial<Order>) {
+    return this.http.post<ApiRes<Order>>(`${this.URL}/seatHold`, order).pipe(
+      map((res: any) => res.data),
+      catchError(this.handleError<Order>('hold seat order'))
+    )
+  }
+
+  confirm(order: Partial<Order>) {
+    return this.http.post<ApiRes<Order>>(`${this.URL}/confirm`, order).pipe(
+      map((res: any) => res.data),
+      catchError(this.handleError<Order>('Confirm order'))
+    )
+  }
 
   private handleError<T>(operation = "operation", result?: T) {
     return (error: any): Observable<T> => {
