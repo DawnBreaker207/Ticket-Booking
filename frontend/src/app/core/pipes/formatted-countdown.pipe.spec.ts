2e154fb0ba07d483b969bbc2ecc9cab912a87cbb
DawnBreaker <tunganhngo207@gmail.com>
2025-09-06 22:23:54 +0700

    feat: add pipe

---
+++ frontend/src/app/core/pipes/formatted-countdown.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FormattedCountdownPipe } from './formatted-countdown.pipe';
+
+describe('FormattedCountdownPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormattedCountdownPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
