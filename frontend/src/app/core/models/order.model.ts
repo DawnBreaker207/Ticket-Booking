dd02099cc5dd4d85912bd1f265eab7e06b6f803f
DawnBreaker <tunganhngo207@gmail.com>
2025-09-15 13:55:01 +0700

    feat: update order model

--- frontend/src/app/core/models/order.model.ts
+++ frontend/src/app/core/models/order.model.ts
@@ -1,6 +1,7 @@
 import {OrderStatus, PaymentMethod, PaymentStatus} from '@/app/core/constants/enum';
+import {DateModel, FilterDate, FilterQuery, FilterSort} from '@/app/core/models/common.model';
 
-export interface Order {
+export interface Order extends DateModel {
   orderId: string;
   userId: number;
   cinemaHallId: number;
@@ -14,4 +15,11 @@ export interface Order {
 export interface OrderSeat {
   seatId: number;
   price: number
+}
+
+export interface OrderFilter extends FilterQuery, FilterSort, FilterDate {
+  orderStatus: OrderStatus;
+  paymentMethod: PaymentMethod;
+  paymentStatus: PaymentStatus;
+  totalAmount: number;
 }
