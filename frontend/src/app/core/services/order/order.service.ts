b44301a6469cefa23d45d13e3717ae19d6db823c
DawnBreaker <tunganhngo207@gmail.com>
2025-09-15 13:55:09 +0700

    feat: update order service

--- frontend/src/app/core/services/order/order.service.ts
+++ frontend/src/app/core/services/order/order.service.ts
@@ -1,10 +1,10 @@
 import {inject, Injectable} from '@angular/core';
 import {environment} from '@/environments/environment';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {catchError, map, Observable, of} from 'rxjs';
 import {ApiRes} from '@/app/core/models/common.model';
-import {Order} from '@/app/core/models/order.model';
-import {CinemaHall} from '@/app/core/models/cinemaHall.model';
+import {Order, OrderFilter} from '@/app/core/models/order.model';
+import {formatTime} from '@/app/shared/utils/formatDate';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +13,27 @@ export class OrderService {
   URL = `${environment.apiUrl}/order`;
   private http = inject(HttpClient);
 
-  getOrders(order: Partial<Order>) {
-    return this.http.get<ApiRes<Order[]>>(`${this.URL}`, {}).pipe(
+  getOrders(filter?: Partial<OrderFilter>, page: number = 0, size: number = 0) {
+    console.log(filter)
+    let params = new HttpParams();
+
+    params = params.set('page', page.toString());
+    params = params.set('size', size.toString());
+
+    const startDate = formatTime(filter?.dateFrom);
+    const endDate = formatTime(filter?.dateTo);
+    filter = {...filter, dateFrom: startDate, dateTo: endDate};
+
+    if (filter) {
+      Object.keys(filter).forEach((key) => {
+        const value = filter[key as keyof OrderFilter];
+        if (value !== null && value !== undefined && value !== '') {
+          params = params.set(key, value.toString());
+        }
+      })
+    }
+
+    return this.http.get<ApiRes<Order[]>>(`${this.URL}`, {params}).pipe(
       map((res: any) => res.data),
       catchError(this.handleError<Order[]>('Get orders'))
     )
@@ -54,4 +73,5 @@ export class OrderService {
       return of(result as T);
     }
   }
+
 }
