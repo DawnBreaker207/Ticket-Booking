2f8478b1c9a7b18f083f60ee379671546833042b
DawnBreaker <tunganhngo207@gmail.com>
2025-10-05 03:33:04 +0700

    feat: update schema

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -6,7 +6,9 @@ CREATE TABLE IF NOT EXISTS users
     password   VARCHAR(255) NOT NULL,
     created_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 CREATE TABLE IF NOT EXISTS movie
 (
     id           INT PRIMARY KEY AUTO_INCREMENT,
@@ -18,9 +20,12 @@ CREATE TABLE IF NOT EXISTS movie
     release_date DATE,
     imdb_id      VARCHAR(255),
     film_id      VARCHAR(255),
+    language     ENUM ('vi', 'en')     DEFAULT 'vi',
     created_at   DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at   DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS cinema_hall
 (
@@ -30,7 +35,9 @@ CREATE TABLE IF NOT EXISTS cinema_hall
     created_at    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_hall_movie FOREIGN KEY (movie_id) REFERENCES movie (id) ON DELETE CASCADE
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS seat
 (
@@ -41,22 +48,26 @@ CREATE TABLE IF NOT EXISTS seat
     status         ENUM ('AVAILABLE', 'BOOKED') DEFAULT 'AVAILABLE',
     CONSTRAINT fk_seat_hall FOREIGN KEY (cinema_hall_id) REFERENCES cinema_hall (id) ON DELETE CASCADE,
     UNIQUE (cinema_hall_id, seat_number)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS orders
 (
-    id             VARCHAR(50) PRIMARY KEY,
+    id             VARCHAR(50)    NOT NULL PRIMARY KEY,
     user_id        BIGINT         NOT NULL,
     cinema_hall_id INT            NOT NULL,
-    status         ENUM ('CREATED','CONFIRMED','CANCELLED')  DEFAULT 'CREATED',
+    status         ENUM ('CREATED','CONFIRMED','CANCELED')   DEFAULT 'CREATED',
     payment_method ENUM ('MOMO', 'VNPAY', 'ZALOPAY', 'CASH') DEFAULT 'CASH',
-    payment_status ENUM ('PENDING', 'PAID', 'CANCELLED')     DEFAULT 'PENDING',
+    payment_status ENUM ('PENDING', 'PAID', 'CANCELED')      DEFAULT 'PENDING',
     total_amount   DECIMAL(10, 2) NOT NULL                   DEFAULT 0.00,
     created_at     DATETIME       NOT NULL                   DEFAULT CURRENT_TIMESTAMP,
     updated_at     DATETIME       NOT NULL                   DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_order_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     CONSTRAINT fk_order_hall FOREIGN KEY (cinema_hall_id) REFERENCES cinema_hall (id) ON DELETE CASCADE
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS order_seat
 (
@@ -64,9 +75,11 @@ CREATE TABLE IF NOT EXISTS order_seat
     order_id VARCHAR(50)    NOT NULL,
     seat_id  INT            NOT NULL,
     price    DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
-    CONSTRAINT fk_order_seat_order FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE,
+    CONSTRAINT fk_order_seat_order FOREIGN KEY (order_id) REFERENCES reservations (id) ON DELETE CASCADE,
     CONSTRAINT fk_order_seat FOREIGN KEY (seat_id) REFERENCES seat (id) ON DELETE CASCADE
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS roles
 (
@@ -75,7 +88,9 @@ CREATE TABLE IF NOT EXISTS roles
     created_at DATETIME NOT NULL                  DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME NOT NULL                  DEFAULT CURRENT_TIMESTAMP
 
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS user_role
 (
@@ -84,7 +99,9 @@ CREATE TABLE IF NOT EXISTS user_role
     PRIMARY KEY (user_id, role_id),
     CONSTRAINT fk_user_role FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     CONSTRAINT fk_role_user FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE IF NOT EXISTS refresh_token
@@ -94,4 +111,21 @@ CREATE TABLE IF NOT EXISTS refresh_token
     token       VARCHAR(512) NOT NULL UNIQUE,
     expiry_date DATETIME     NOT NULL,
     CONSTRAINT fk_user_refresh FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS payment
+(
+    id                BIGINT PRIMARY KEY AUTO_INCREMENT,
+    reservation_id    VARCHAR(50)    NOT NULL,
+    payment_intent_id VARCHAR(255)   NOT NULL,
+    amount            DECIMAL(10, 2) NOT NULL             DEFAULT 0.00,
+    status            ENUM ('PENDING', 'PAID','CANCELED') DEFAULT 'USER',
+    created_at        DATETIME       NOT NULL             DEFAULT CURRENT_TIMESTAMP,
+    updated_at        DATETIME       NOT NULL             DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_payment FOREIGN KEY (reservation_id) REFERENCES reservations (id) ON DELETE CASCADE
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
