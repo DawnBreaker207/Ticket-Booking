caa329b1b50ea4a8f38bd3c5cb9b6e1df52fbf49
DawnBreaker <tunganhngo207@gmail.com>
2025-10-11 01:00:56 +0700

    feat: update notification feature

---
+++ backend/src/main/java/com/example/backend/controller/NotificationController.java
@@ -0,0 +1,21 @@
+package com.example.backend.controller;
+
+import com.example.backend.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mail")
+@RequiredArgsConstructor
+public class NotificationController {
+    private final NotificationService notificationService;
+
+
+    @GetMapping("/test")
+    public void sendEmail() {
+        notificationService.sendEmail("ngotunganh207@gmail.com", "test", "ORD-D4DAAD940C10");
+    }
+
+}
---
+++ backend/src/main/java/com/example/backend/service/NotificationService.java
@@ -0,0 +1,51 @@
+package com.example.backend.service;
+
+import com.example.backend.util.BarcodeUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+
+    private final JavaMailSender mailSender;
+
+    private final TemplateEngine templateEngine;
+
+    public void sendEmail(String to, String name, String reservationId) {
+        log.info("Got message from reservation");
+        String barcodeBase64 = BarcodeUtils.generateCode128(reservationId, 300, 100);
+
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+            messageHelper.setFrom("demo@gmail.com");
+            messageHelper.setTo(to);
+            messageHelper.setSubject("Your reservation with id is place successfully");
+
+            Context context = new Context();
+            context.setVariable("name", name);
+            context.setVariable("barcode", barcodeBase64);
+
+            String html = templateEngine.process("email", context);
+            messageHelper.setText(html, true);
+        };
+
+        try {
+            mailSender.send(messagePreparator);
+            log.info("Email notification sent!");
+        } catch (MailException ex) {
+            log.error("Exception occurred when sending email", ex);
+            throw new RuntimeException("Exception occurred when sending email to demo@gmail.com", ex);
+        }
+
+    }
+
+}
