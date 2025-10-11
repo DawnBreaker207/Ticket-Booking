1a62aca986a07d6517a356bb9d43de1263c18ccf
DawnBreaker <tunganhngo207@gmail.com>
2025-10-11 20:45:16 +0700

    feat: update rate limit

--- backend/src/main/java/com/example/backend/config/RateLimitFilter.java
+++ backend/src/main/java/com/example/backend/config/RateLimitFilter.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
