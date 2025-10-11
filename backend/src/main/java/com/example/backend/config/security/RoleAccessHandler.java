333bb70cf2514606b3cb30df0e30a23bdabf2199
DawnBreaker <tunganhngo207@gmail.com>
2025-10-11 11:23:15 +0700

    feat: update role access config

--- backend/src/main/java/com/example/backend/config/security/RoleAccessHandler.java
+++ backend/src/main/java/com/example/backend/config/security/RoleAccessHandler.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -13,12 +14,10 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class RoleAccessHandler implements AccessDeniedHandler {
-    private final ObjectMapper objectMapper;
 
-    public RoleAccessHandler(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
+    private final ObjectMapper objectMapper;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
