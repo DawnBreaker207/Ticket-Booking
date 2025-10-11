3161a39be1a213e038d6fc55b9f05e628a33e0df
DawnBreaker <tunganhngo207@gmail.com>
2025-10-11 20:44:31 +0700

    feat: update user repository

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -1,26 +1,13 @@
 package com.example.backend.repository;
 
 import com.example.backend.model.User;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    @Modifying
-    @Transactional
-    @Query(value = "UPDATE users u SET u.username = :#{#user.username}, u.email = :#{#user.email}, u.password = :#{#user.password} ", nativeQuery = true)
-    int update(User user);
-
-    @Modifying
-    @Transactional
-    @Query(value = "INSERT INTO user_role (user_id, role_id) VALUES (:userId, :roleId)", nativeQuery = true)
-    void insertUserRoles( Long userId, Long roleId);
 
     Optional<User> findByEmail(String email);
 
