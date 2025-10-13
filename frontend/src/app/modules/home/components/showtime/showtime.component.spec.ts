6c3e4327e6636201e4516d038c3a51a9b3b070c9
DawnBreaker <tunganhngo207@gmail.com>
2025-10-13 15:43:42 +0700

    feat: update user showtime component in FE

--- frontend/src/app/modules/home/components/showtime/showtime.component.spec.ts
+++ frontend/src/app/modules/home/components/showtime/showtime.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ScheduleComponent } from './schedule.component';
+import { ShowtimeComponent } from './showtime.component';
 
-describe('ScheduleComponent', () => {
-  let component: ScheduleComponent;
-  let fixture: ComponentFixture<ScheduleComponent>;
+describe('ShowtimeComponent', () => {
+  let component: ShowtimeComponent;
+  let fixture: ComponentFixture<ShowtimeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ScheduleComponent]
+      imports: [ShowtimeComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ScheduleComponent);
+    fixture = TestBed.createComponent(ShowtimeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
