04d7db1f4197932c8a763cc707df8b8976cb8c97
DawnBreaker <tunganhngo207@gmail.com>
2025-10-13 16:04:51 +0700

    feat: update admin routing in FE

--- frontend/src/app/modules/admin/admin-routing-module.ts
+++ frontend/src/app/modules/admin/admin-routing-module.ts
@@ -3,17 +3,18 @@ import {RouterModule, Routes} from '@angular/router';
 import {AdminComponent} from '@/app/modules/admin/admin.component';
 import {DashboardComponent} from '@/app/modules/admin/components/dashboard/dashboard.component';
 import {MovieComponent} from '@/app/modules/admin/components/movie/movie.component';
-import {OrderComponent} from '@/app/modules/admin/components/order/order.component';
-import {ScheduleComponent} from '@/app/modules/admin/components/schedule/schedule.component';
-import {FormMovieAPIComponent} from '@/app/modules/admin/components/movie/form/api/form.component';
+import {ReservationComponent} from '@/app/modules/admin/components/reservation/reservation.component';
+import {TheaterComponent} from '@/app/modules/admin/components/theater/theater.component';
+import {ShowtimeComponent} from '@/app/modules/admin/components/showtime/showtime.component';
 
 const routes: Routes = [
     {
       path: '', component: AdminComponent, children: [
         {path: 'dashboard', component: DashboardComponent},
         {path: 'movie', component: MovieComponent},
-        {path: 'schedule', component: ScheduleComponent},
-        {path: 'order', component: OrderComponent},
+        {path: 'theater', component: TheaterComponent},
+        {path: 'showtime', component: ShowtimeComponent},
+        {path: 'reservation', component: ReservationComponent},
       ]
     }, {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
     {path: '**', redirectTo: 'errors/404'}
