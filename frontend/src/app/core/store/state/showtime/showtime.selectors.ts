24ab3a4b028c87cbeb9cb14448f9623f2a9ab9a0
DawnBreaker <tunganhngo207@gmail.com>
2025-10-13 15:43:17 +0700

    feat: update showtime admin feature in FE

---
+++ frontend/src/app/core/store/state/showtime/showtime.selectors.ts
@@ -0,0 +1,52 @@
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+import {showtimeFeatureKey, ShowtimeState} from '@/app/core/store/state/showtime/showtime.reducers';
+
+export const selectShowtimeState = createFeatureSelector<ShowtimeState>(showtimeFeatureKey);
+
+
+export const selectAllShowtimes =
+  createSelector(
+    selectShowtimeState,
+    (state) => state.showtimes);
+
+export const selectSelectedShowtime =
+  createSelector(
+    selectShowtimeState,
+    (state) => state.selectedShowtime);
+
+export const selectShowtimeById =
+  (id: number) =>
+    createSelector(selectAllShowtimes,
+      (showtimes) =>
+        showtimes.find(m => m.id === id) || null);
+
+// Loading state selector
+export const selectShowtimeLoading =
+  createSelector(
+    selectShowtimeState,
+    (state) => state.loading);
+
+export const selectShowtimeLoadingDetails =
+  createSelector(
+    selectShowtimeState,
+    (state) => state.loadingDetails);
+
+export const selectShowtimeSaving =
+  createSelector(
+    selectShowtimeState,
+    (state) => state.saving);
+
+// Error Selector
+export const selectShowtimeError =
+  createSelector(
+    selectShowtimeState,
+    (state) => state.error);
+export const selectShowtimeCount =
+  createSelector(
+    selectAllShowtimes,
+    (movies) => movies.length);
+
+export const selectHasShowtime =
+  createSelector(
+    selectAllShowtimes,
+    (movies) => movies.length > 0);
