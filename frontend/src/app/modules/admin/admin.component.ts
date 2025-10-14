a0c600abfee715b51e6bbd2874e3328adac2fb46
DawnBreaker <tunganhngo207@gmail.com>
2025-10-14 23:41:41 +0700

    feat: update admin component in FE

--- frontend/src/app/modules/admin/admin.component.ts
+++ frontend/src/app/modules/admin/admin.component.ts
@@ -1,14 +1,14 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import {
   NzContentComponent,
   NzFooterComponent,
   NzHeaderComponent,
   NzLayoutComponent,
-  NzSiderComponent
-} from "ng-zorro-antd/layout";
-import {NzIconDirective} from "ng-zorro-antd/icon";
-import {NzMenuDirective, NzMenuItemComponent} from "ng-zorro-antd/menu";
-import {RouterLink, RouterOutlet} from "@angular/router";
+  NzSiderComponent,
+} from 'ng-zorro-antd/layout';
+import { NzIconDirective } from 'ng-zorro-antd/icon';
+import { NzMenuDirective, NzMenuItemComponent } from 'ng-zorro-antd/menu';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -22,10 +22,10 @@ import {RouterLink, RouterOutlet} from "@angular/router";
     NzSiderComponent,
     RouterLink,
     RouterOutlet,
-    NzFooterComponent
+    NzFooterComponent,
   ],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrl: './admin.component.css',
 })
 export class AdminComponent {
   isCollapsed = false;
