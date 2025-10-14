2a7fc6b6fa49d49e65f7ee14f0192a5c547c5105
DawnBreaker <tunganhngo207@gmail.com>
2025-10-14 14:10:11 +0700

    feat: update column table in FE

--- frontend/src/app/core/constants/column.ts
+++ frontend/src/app/core/constants/column.ts
@@ -1,10 +1,38 @@
 export const headerColumns = {
-  movie: ['#', 'Title', 'Poster', 'Overview', 'Genre', 'Duration', 'Release', 'Action'],
-  theater: ['#', 'Movie', "Poster", 'Movie Release', 'Action'],
-  showtime: ['#', 'Movie', "Poster", 'Movie Release', 'Action'],
-  reservation: ['#', 'Status', 'Total', 'Day Create','Is Deleted', 'Action']
-}
+  movie: [
+    '#',
+    'Title',
+    'Poster',
+    'Overview',
+    'Genre',
+    'Duration',
+    'Release',
+    'Action',
+  ],
+  theater: ['#', 'Name', 'Location', 'Capacity', 'Day Created', 'Action'],
+  showtime: [
+    '#',
+    'Movie',
+    'Poster',
+    'Showtime',
+    'Show date',
+    'Theater',
+    'Location',
+    'Seat Available',
+    'Action',
+  ],
+  reservation: ['#', 'Status', 'Total', 'Day Create', 'Action'],
+};
 
 export const displayColumns = {
-  movie: ['id', 'title', 'poster', 'overview', 'genre', 'duration', 'release', 'action'],
-}
+  movie: [
+    'id',
+    'title',
+    'poster',
+    'overview',
+    'genre',
+    'duration',
+    'release',
+    'action',
+  ],
+};
