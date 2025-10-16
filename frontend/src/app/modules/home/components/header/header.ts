895d675723b0d356a0723219390854faeed2c175
DawnBreaker <tunganhngo207@gmail.com>
2025-10-16 14:34:25 +0700

    feat: update home page in FE

--- frontend/src/app/modules/home/components/header/header.ts
+++ frontend/src/app/modules/home/components/header/header.ts
@@ -1,31 +1,23 @@
-import {Component} from '@angular/core';
-import {UpperCasePipe} from '@angular/common';
-import {RouterLink} from '@angular/router';
-import {NzButtonComponent} from 'ng-zorro-antd/button';
-import {NzMenuDirective} from 'ng-zorro-antd/menu';
+import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { NzMenuDirective } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-header',
-  imports: [
-    UpperCasePipe,
-    RouterLink,
-    NzButtonComponent,
-    NzMenuDirective,
-
-  ],
+  imports: [UpperCasePipe, RouterLink, NzButtonComponent, NzMenuDirective],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
 })
 export class HeaderComponent {
   headers = [
-    {name: "Lịch chiếu rạp", path: ""},
-    {name: "Phim", path: "/home"},
-    {name: " Rạp", path: ""},
-    {name: "Giá vé", path: ""},
-    {name: "Tin mới và ưu đãi", path: ""},
-    {name: "Nhượng quyền", path: ""},
-    {name: "Thành viên", path: "/login"}
-
-
+    { name: 'Lịch chiếu rạp', path: '' },
+    { name: 'Phim', path: '/home' },
+    { name: ' Rạp', path: '' },
+    { name: 'Giá vé', path: '' },
+    { name: 'Tin mới và ưu đãi', path: '' },
+    { name: 'Nhượng quyền', path: '' },
+    { name: 'Thành viên', path: '/login' },
   ];
 }
--- frontend/src/app/modules/home/pages/home/home.ts
+++ frontend/src/app/modules/home/pages/home/home.ts
@@ -1,31 +1,32 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzTypographyComponent } from 'ng-zorro-antd/typography';
-import {
-  NzColDirective,
-  NzGridModule,
-  NzRowDirective,
-} from 'ng-zorro-antd/grid';
-import { NzCardComponent, NzCardModule } from 'ng-zorro-antd/card';
-import { NzImageViewComponent } from 'ng-zorro-antd/experimental/image';
-import { NzButtonComponent } from 'ng-zorro-antd/button';
-import { NzImageService } from 'ng-zorro-antd/image';
-import { Router, RouterLink } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { filter, forkJoin, Subject, take, takeUntil } from 'rxjs';
-import { Jwt } from '@/app/core/models/jwt.model';
-import { TheaterActions } from '@/app/core/store/state/theater/theater.actions';
-import { NzModalService } from 'ng-zorro-antd/modal';
-import { TheaterComponent } from '@/app/modules/home/components/theater/theater.component';
-import { selectAllShowtimes } from '@/app/core/store/state/showtime/showtime.selectors';
-import { selectToken } from '@/app/core/store/state/auth/auth.selectors';
-import { selectAllTheaters } from '@/app/core/store/state/theater/theater.selectors';
-import { Showtime } from '@/app/core/models/theater.model';
-import { ShowtimeComponent } from '@/app/modules/home/components/showtime/showtime.component';
-import { ShowtimeActions } from '@/app/core/store/state/showtime/showtime.actions';
-import { Movie } from '@/app/core/models/movie.model';
-import { selectMovieById } from '@/app/core/store/state/movie/movie.selectors';
-import { MovieActions } from '@/app/core/store/state/movie/movie.actions';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {NzLayoutModule} from 'ng-zorro-antd/layout';
+import {NzTypographyComponent} from 'ng-zorro-antd/typography';
+import {NzColDirective, NzGridModule, NzRowDirective,} from 'ng-zorro-antd/grid';
+import {NzCardComponent, NzCardModule} from 'ng-zorro-antd/card';
+import {NzImageViewComponent} from 'ng-zorro-antd/experimental/image';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
+import {NzImageService} from 'ng-zorro-antd/image';
+import {Router, RouterLink} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {filter, forkJoin, map, Subject, switchMap, take, takeUntil} from 'rxjs';
+import {Jwt} from '@/app/core/models/jwt.model';
+import {TheaterActions} from '@/app/core/store/state/theater/theater.actions';
+import {NzModalService} from 'ng-zorro-antd/modal';
+import {TheaterComponent} from '@/app/modules/home/components/theater/theater.component';
+import {selectAllShowtimes} from '@/app/core/store/state/showtime/showtime.selectors';
+import {selectJwt,} from '@/app/core/store/state/auth/auth.selectors';
+import {selectAllTheaters} from '@/app/core/store/state/theater/theater.selectors';
+import {Showtime} from '@/app/core/models/theater.model';
+import {ShowtimeComponent} from '@/app/modules/home/components/showtime/showtime.component';
+import {ShowtimeActions} from '@/app/core/store/state/showtime/showtime.actions';
+import {Movie} from '@/app/core/models/movie.model';
+import {selectMovieById} from '@/app/core/store/state/movie/movie.selectors';
+import {MovieActions} from '@/app/core/store/state/movie/movie.actions';
+import {ReservationInitRequest,} from '@/app/core/models/reservation.model';
+import {UserService} from '@/app/core/services/user/user.service';
+import {ReservationActions} from '@/app/core/store/state/reservation/reservation.actions';
+import {Actions, ofType} from '@ngrx/effects';
+import {User} from '@/app/core/models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -48,12 +49,14 @@ import { MovieActions } from '@/app/core/store/state/movie/movie.actions';
 export class Home implements OnInit, OnDestroy {
   private store = inject(Store);
   private router = inject(Router);
+  private actions$ = inject(Actions);
   private modalService = inject(NzModalService);
+  private userService = inject(UserService);
   private destroy$ = new Subject<void>();
+
   showtimes$ = this.store.select(selectAllShowtimes);
   theaters$ = this.store.select(selectAllTheaters);
-  user$ = this.store.select(selectToken);
-
+  user$ = this.store.select(selectJwt);
   selectedTheaterId: number | null = null;
   showTimesForTheater: Showtime[] = [];
   movies: Movie[] = [];
@@ -68,10 +71,7 @@ export class Home implements OnInit, OnDestroy {
   }
 
   private selectTheaterModal() {
-    const movieId = 1;
-
     this.store.dispatch(TheaterActions.loadTheaters());
-    this.store.dispatch(ShowtimeActions.loadShowtimes({ movieId: movieId }));
     this.store.dispatch(MovieActions.loadMovies());
     this.theaters$
       .pipe(
@@ -82,19 +82,31 @@ export class Home implements OnInit, OnDestroy {
       .subscribe((theaters) => {
         const defaultTheater = theaters[0];
         this.selectedTheaterId = defaultTheater.id;
+        this.store.dispatch(
+          ShowtimeActions.loadShowtimesByTheaterId({
+            theaterId: defaultTheater.id as number,
+          }),
+        );
+
         this.filterShowtimeByTheater(defaultTheater.id);
-        this.modalService.create({
+        const modal = this.modalService.create({
           nzTitle: 'Chọn rạp chiếu',
           nzContent: TheaterComponent,
           nzData: {
             theaters: theaters,
             selectedTheaterId: this.selectedTheaterId,
           },
-          nzOnOk: (instance) => {
-            this.selectedTheaterId = instance.selectedTheater();
-            this.filterShowtimeByTheater(this.selectedTheaterId as number);
-            this.updateMovieForHome();
-          },
+          nzFooter: null,
+        });
+        modal.afterClose.subscribe((selectedId: number | null) => {
+          if (!selectedId) return;
+
+          this.selectedTheaterId = selectedId;
+          this.store.dispatch(
+            ShowtimeActions.loadShowtimesByTheaterId({theaterId: selectedId}),
+          );
+          this.filterShowtimeByTheater(selectedId);
+          this.updateMovieForHome();
         });
       });
   }
@@ -162,33 +174,54 @@ export class Home implements OnInit, OnDestroy {
       );
 
       if (!showtimeForMovies.length) return;
-      this.modalService.create({
-        nzTitle: `Chọn khung giờ`,
-        nzContent: ShowtimeComponent,
-        nzData: {
-          showtimes: showtimeForMovies,
-        },
+      this.modalService
+        .create({
+          nzTitle: `Chọn khung giờ`,
+          nzContent: ShowtimeComponent,
+          nzData: {
+            showtimes: showtimeForMovies,
+          },
+          nzFooter: null,
+        })
+        .afterClose.subscribe((selectShowtimeId: number | null) => {
+        if (!selectShowtimeId) return;
+        this.proceedToReservation(selectShowtimeId);
       });
     });
   }
 
   private proceedToReservation(showtimeId: number) {
-    if (!this.selectedTheaterId) {
-      this.modalService.warning({
-        nzTitle: 'Error',
-        nzContent: 'Please select a theater',
-      });
-      return;
-    }
-
     this.user$
       .pipe(
         take(1),
         filter((jwt): jwt is Jwt => jwt !== undefined),
+        switchMap((res) => this.userService.getByEmail(res.email)),
+        filter((user): user is User => !!user),
+        switchMap((user) => {
+          console.log(user)
+          const reservation: ReservationInitRequest = {
+            reservationId: '',
+            userId: user.userId.toString(),
+            showtimeId: showtimeId,
+            theaterId: this.selectedTheaterId!,
+          };
+          console.log('Init Reservation', reservation)
+
+          this.store.dispatch(
+            ReservationActions.createReservationInit({reservation: reservation}),
+          );
+
+          return this.actions$
+            .pipe(
+              ofType(ReservationActions.createReservationSuccessInit),
+              take(1),
+              map(({reservationId}) => reservationId)
+            )
+        }),
         takeUntil(this.destroy$),
       )
-      .subscribe((res) => {
-        this.router.navigateByUrl(`/reservation/${res}`);
+      .subscribe((reservationId) => {
+        this.router.navigateByUrl(`/reservation/${reservationId}`);
       });
   }
 }
