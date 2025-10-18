4cd296b1a4404b443a3bd9c402b0b217cb1c5fd2
DawnBreaker <tunganhngo207@gmail.com>
2025-10-18 15:20:13 +0700

    feat: update countdown feature in FE

--- frontend/src/app/shared/utils/formatDate.ts
+++ frontend/src/app/shared/utils/formatDate.ts
@@ -4,8 +4,14 @@ export const formatDate = (date?: Date | string): string | undefined => {
   if (!date) return undefined;
   return dayjs(date).format('YYYY-MM-DD').toString();
 };
-export const formatTime =
-  (date?: Date | string): string | undefined => {
-    if (!date) return undefined;
-    return dayjs(date).format('HH:mm:ss').toString();
-  }
+export const formatTime = (date?: Date | string): string | undefined => {
+  if (!date) return undefined;
+  return dayjs(date).format('HH:mm:ss').toString();
+};
+export const timeFormat = (time: Date | string | null | undefined) => {
+  if (!time) return '-';
+  const today = dayjs().format('YYYY-MM-DD');
+  const dateTime = dayjs(`${today} ${time}`, 'YYYY-MM-DD HH:mm:ss');
+  if (!dateTime.isValid()) return '-';
+  return dateTime.format('HH:mm');
+}
