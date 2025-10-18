35f59735f1bc7248c1f9026c6e2bc5d346e7c38d
DawnBreaker <tunganhngo207@gmail.com>
2025-10-18 15:19:19 +0700

    feat: update reservation admin in FE

--- frontend/src/app/modules/home/components/reservation/detail-film/detail-film.component.ts
+++ frontend/src/app/modules/home/components/reservation/detail-film/detail-film.component.ts
@@ -1,11 +1,18 @@
-import { Component, inject, input, output } from '@angular/core';
-import { Movie } from '@/app/core/models/movie.model';
-import { NzImageModule } from 'ng-zorro-antd/image';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzImageViewComponent } from 'ng-zorro-antd/experimental/image';
-import { NzButtonComponent } from 'ng-zorro-antd/button';
-import { NzWaveDirective } from 'ng-zorro-antd/core/wave';
-import { Store } from '@ngrx/store';
+import {Component, inject, input, OnInit, output} from '@angular/core';
+import {NzImageModule} from 'ng-zorro-antd/image';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {NzImageViewComponent} from 'ng-zorro-antd/experimental/image';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
+import {NzWaveDirective} from 'ng-zorro-antd/core/wave';
+import {Store} from '@ngrx/store';
+import {selectSelectedMovie} from '@/app/core/store/state/movie/movie.selectors';
+import {CommonModule} from '@angular/common';
+import {selectSelectedTheater} from '@/app/core/store/state/theater/theater.selectors';
+import {selectSelectedSeats} from '@/app/core/store/state/seat/seat.selectors';
+import {map} from 'rxjs';
+import {selectSelectedShowtime} from '@/app/core/store/state/showtime/showtime.selectors';
+import {timeFormat} from '@/app/shared/utils/formatDate';
+import {MovieActions} from '@/app/core/store/state/movie/movie.actions';
 
 @Component({
   selector: 'app-detail-film',
@@ -15,20 +22,33 @@ import { Store } from '@ngrx/store';
     NzImageViewComponent,
     NzButtonComponent,
     NzWaveDirective,
+    CommonModule,
   ],
   templateUrl: './detail-film.component.html',
   styleUrl: './detail-film.component.css',
 })
-export class DetailFilmComponent {
-  filmDetail = input<Movie | null | undefined>(null);
-  // cinemaHall = input<CinemaHall | null | undefined>(null);
+export class DetailFilmComponent implements OnInit {
+  private store = inject(Store);
+  filmDetail$ = this.store.select(selectSelectedMovie);
+  theater$ = this.store.select(selectSelectedTheater);
+  showtime$ = this.store.select(selectSelectedShowtime);
   index = input<number>(0);
   steps = input<number[]>([]);
-  store = inject(Store);
-  // selectedSeats$ = this.store
-  //   .select(selectedSeats)
-  //   .pipe(map((seats) => seats.map((s) => s.seatNumber).join(', ')));
+  selectedSeats$ = this.store
+    .select(selectSelectedSeats).pipe(
+      map((seats) =>
+        seats.map((s) => s.seatNumber)
+          .join(', ')));
+
   stepChange = output<number>();
+
+  ngOnInit() {
+    this.showtime$.subscribe((data) =>
+      this.store.dispatch(
+        MovieActions.loadMovie({id: data?.movieId as number}),
+      ),
+    );
+  }
 
   nextStep() {
     if (this.index() < this.steps().length - 1) {
@@ -45,4 +65,6 @@ export class DetailFilmComponent {
   saveSeats() {
     this.stepChange.emit(this.index() + 1);
   }
+
+  protected readonly timeFormat = timeFormat;
 }
