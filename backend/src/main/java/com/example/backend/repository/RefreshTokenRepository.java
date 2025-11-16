7e509e3bb57685f9faf72767e50a3c42d5ed9dc8
DawnBreaker <tunganhngo207@gmail.com>
2025-11-17 01:08:55 +0700

    feat: update refresh token feature

--- backend/src/main/java/com/example/backend/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/example/backend/repository/RefreshTokenRepository.java
@@ -11,9 +11,6 @@ import java.util.Optional;
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
-//    @Modifying
-//    @Transactional
-//    @Query(value = "DELETE FROM refresh_token WHERE token = :token", nativeQuery = true)
     void deleteByToken(String token);
 
     Optional<RefreshToken> findByToken(String token);
