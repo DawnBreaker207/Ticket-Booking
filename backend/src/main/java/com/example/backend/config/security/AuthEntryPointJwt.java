2cbd45289d7b5f0e80fa7d857a98ce57be6b8c1c
DawnBreaker <tunganhngo207@gmail.com>
2025-11-17 02:16:23 +0700

    feat: update auth entry point

--- backend/src/main/java/com/example/backend/config/security/AuthEntryPointJwt.java
+++ backend/src/main/java/com/example/backend/config/security/AuthEntryPointJwt.java
@@ -1,37 +1,38 @@
 package com.example.backend.config.security;
 
+import com.example.backend.config.response.ResponseObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
+    private final ObjectMapper objectMapper;
+
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         log.error("Unauthorized error: {}", authException.getMessage());
 
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
-        final Map<String, Object> body = new HashMap<>();
-        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
-        body.put("error", "Unauthorized");
-        body.put("message", authException.getMessage());
-        body.put("path", request.getServletPath());
-
-        final ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(response.getOutputStream(), body);
+        ResponseObject<String> resObj = new ResponseObject<>(
+                HttpStatus.UNAUTHORIZED,
+                "Unauthorized",
+                authException.getMessage()
+        );
+        response.getWriter().write(objectMapper.writeValueAsString(resObj.getBody()));
     }
 }
