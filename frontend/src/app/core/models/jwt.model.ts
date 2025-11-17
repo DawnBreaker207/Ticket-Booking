3de8c334fdf67eb9de13834c41c1545b2d5f09bf
DawnBreaker <tunganhngo207@gmail.com>
2025-11-17 15:36:14 +0700

    feat: update model

--- frontend/src/app/core/models/jwt.model.ts
+++ frontend/src/app/core/models/jwt.model.ts
@@ -1,12 +1,17 @@
+import { JwtPayload as JWTDecodePayload } from 'jwt-decode';
+
 export interface Jwt {
   userId: number;
   username: string;
   email: string;
   accessToken: string;
-  refreshToken: string;
+}
+
+export interface JWTPayload extends JWTDecodePayload {
+  email: string;
+  roles: string[];
 }
 
 export interface RefreshToken {
   accessToken: string;
-  refreshToken: string;
 }
