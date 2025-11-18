6e546d149b41dedb17a8f7dd7f106a12b45561e0
DawnBreaker <tunganhngo207@gmail.com>
2025-11-18 14:14:41 +0700

    feat: update profile menu component

--- frontend/src/app/shared/components/profile-menu/profile-menu.component.ts
+++ frontend/src/app/shared/components/profile-menu/profile-menu.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import {
   CircleUserRound,
   LogOut,
@@ -7,17 +7,25 @@ import {
   Settings,
 } from 'lucide-angular';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { Store } from '@ngrx/store';
+import { selectJwt } from '@/app/core/store/state/auth/auth.selectors';
+import { AsyncPipe } from '@angular/common';
+import { AuthActions } from '@/app/core/store/state/auth/auth.actions';
 
 @Component({
   selector: 'app-profile-menu',
-  imports: [RouterLink, LucideAngularModule, NzDropDownModule],
+  imports: [LucideAngularModule, NzDropDownModule, AsyncPipe],
   templateUrl: './profile-menu.component.html',
   styleUrl: './profile-menu.component.css',
 })
 export class ProfileMenuComponent {
+  private store = inject(Store);
+  router = inject(Router);
+  user$ = this.store.select(selectJwt);
   readonly CircleUserRound = CircleUserRound;
   readonly Settings = Settings;
   readonly LogOut = LogOut;
+
   public profileMenu = [
     {
       title: 'Your Profile',
@@ -32,7 +40,11 @@ export class ProfileMenuComponent {
     {
       title: 'Log out',
       icon: this.LogOut,
-      link: '/auth',
+      action: () => this.logout(),
     },
   ];
+
+  logout() {
+    this.store.dispatch(AuthActions.loadLogout());
+  }
 }
