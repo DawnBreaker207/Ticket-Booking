d172deba00a642b2adee0ee825807cab3fb0a0ee
DawnBreaker <tunganhngo207@gmail.com>
2025-11-18 14:08:34 +0700

    feat: update cors config

--- backend/src/main/java/com/dawn/backend/config/security/CorsConfig.java
+++ backend/src/main/java/com/dawn/backend/config/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.dawn.backend.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,23 +14,32 @@ public class CorsConfig {
             "http://localhost:3000",
             "http://localhost:4200",
             "http://localhost:5173"};
-    public UrlBasedCorsConfigurationSource config(){
+
+    @Bean
+    public UrlBasedCorsConfigurationSource config() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOrigins(Arrays.asList(ALLOWED_DOMAINS));
-        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        config.setAllowedHeaders(Arrays.asList(
+                "Authorization",
+                "Cache-Control",
+                "Content-Type",
+                "X-Requested-With",
+                "Accept",
+                "Origin"
+        ));
+
         config.setAllowedMethods(Arrays.asList(
                 HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
                 HttpMethod.PUT.name(),
+                HttpMethod.PATCH.name(),
                 HttpMethod.DELETE.name(),
                 HttpMethod.OPTIONS.name()));
         config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        source.registerCorsConfiguration("/api/v1/**", config);
-        source.registerCorsConfiguration("/api/v1/notification/**", config);
         return source;
     }
 }
