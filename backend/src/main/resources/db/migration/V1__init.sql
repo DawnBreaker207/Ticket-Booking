5e238c1713d6f7ce5bf034a48daa74838842e04a
DawnBreaker <tunganhngo207@gmail.com>
2025-11-25 00:36:41 +0700

    feat: update schema

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    username   VARCHAR(255) NOT NULL,
+    username   VARCHAR(255) NOT NULL UNIQUE,
     email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
+    avatar     VARCHAR(255),
     is_deleted BOOLEAN               DEFAULT FALSE,
     created_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -29,8 +30,8 @@ CREATE TABLE IF NOT EXISTS user_role
     user_id BIGINT NOT NULL,
     role_id BIGINT NOT NULL,
     PRIMARY KEY (user_id, role_id),
-    CONSTRAINT fk_user_role FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT fk_role_user FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+    CONSTRAINT fk_user_role_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_user_role_role FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
@@ -58,8 +59,8 @@ CREATE TABLE IF NOT EXISTS movie
 
 CREATE TABLE IF NOT EXISTS genre
 (
-    id   BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(255) NOT NULL UNIQUE,
     created_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE = InnoDB
@@ -77,7 +78,7 @@ CREATE TABLE IF NOT EXISTS movie_genre
 CREATE TABLE IF NOT EXISTS theater
 (
     id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name       VARCHAR(255) NOT NULL,
+    name       VARCHAR(255) NOT NULL UNIQUE,
     location   VARCHAR(255),
     capacity   INT          NOT NULL,
     is_deleted BOOLEAN               DEFAULT FALSE,
@@ -153,7 +154,7 @@ CREATE TABLE IF NOT EXISTS payment
     reservation_id    VARCHAR(50) UNIQUE,
     payment_intent_id VARCHAR(255)   NOT NULL UNIQUE,
     amount            DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
-    method            ENUM ('MOMO', 'VNPAY', 'ZALOPAY'),
+    method            ENUM ('MOMO', 'VNPAY'),
     status            ENUM ('PAID', 'CANCELED'),
     created_at        DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at        DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
