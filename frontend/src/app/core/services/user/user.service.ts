0769a27e7246b573d7bf9833952a8091dd497c5a
DawnBreaker <tunganhngo207@gmail.com>
2025-11-25 14:52:25 +0700

    feat: update user service

--- frontend/src/app/core/services/user/user.service.ts
+++ frontend/src/app/core/services/user/user.service.ts
@@ -1,9 +1,10 @@
 import { inject, Injectable } from '@angular/core';
 import { catchError, map, Observable, of } from 'rxjs';
 import { environment } from '@/environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { ApiRes } from '@/app/core/models/common.model';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { ApiRes, ResponsePage } from '@/app/core/models/common.model';
 import { User } from '@/app/core/models/user.model';
+import { ReservationFilter } from '@/app/core/models/reservation.model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,10 +13,19 @@ export class UserService {
   URL = `${environment.apiUrl}/user`;
   private http = inject(HttpClient);
 
-  getALl() {
-    return this.http.get<ApiRes<User>>(`${this.URL}`).pipe(
+  getALl(filter?: any) {
+    let params = new HttpParams();
+    if (filter) {
+      Object.keys(filter).forEach((key) => {
+        const value = filter[key as keyof ReservationFilter];
+        if (value !== null && value !== undefined && value !== '') {
+          params = params.set(key, value.toString());
+        }
+      });
+    }
+    return this.http.get<ApiRes<ResponsePage<User[]>>>(`${this.URL}`).pipe(
       map((res) => res.data),
-      catchError(this.handleError<User>('Get users')),
+      catchError(this.handleError<ResponsePage<User[]>>('Get users')),
     );
   }
 
