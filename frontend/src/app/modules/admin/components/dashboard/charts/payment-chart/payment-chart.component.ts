a37206d1d8a78006c891e1ea905d5d39fcb8ebb6
DawnBreaker <tunganhngo207@gmail.com>
2025-11-29 01:39:12 +0700

    feat: update payment chart component

--- frontend/src/app/modules/admin/components/dashboard/charts/payment-chart/payment-chart.component.ts
+++ frontend/src/app/modules/admin/components/dashboard/charts/payment-chart/payment-chart.component.ts
@@ -3,10 +3,12 @@ import type { EChartsCoreOption } from 'echarts/core';
 import { NgxEchartsDirective } from 'ngx-echarts';
 import { PaymentDistribution } from '@/app/core/models/dashboard.model';
 import { DashboardService } from '@/app/core/services/dashboard/dashboard.service';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-payment-chart',
-  imports: [NgxEchartsDirective],
+  imports: [NgxEchartsDirective, NzIconModule, NzButtonModule],
   templateUrl: './payment-chart.component.html',
   styleUrl: './payment-chart.component.css',
 })
@@ -33,14 +35,20 @@ export class PaymentChartComponent implements OnInit {
       tooltip: {
         trigger: 'item',
       },
+      legend: { bottom: '0%', left: 'center', icon: 'circle' },
       series: [
         {
+          name: 'Payment Type',
           type: 'pie',
-          radius: '60%',
+          radius: ['40%', '70%'],
+          center: ['50%', '45%'],
+          itemStyle: { borderRadius: 2, borderColor: '#fff', borderWidth: 2 },
+          label: { show: false },
           data: data,
         },
       ],
       emphasis: {
+        label: { show: true, fontSize: 12, fontWeight: 'bold' },
         itemStyle: {
           shadowBlur: 10,
           shadowOffsetX: 0,
