8f0e17215af966652f0c9d127240f9700a177d32
DawnBreaker <tunganhngo207@gmail.com>
2025-11-29 15:31:26 +0700

    feat: update footer

--- frontend/src/app/modules/home/components/footer/footer.component.ts
+++ frontend/src/app/modules/home/components/footer/footer.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMenuDirective } from 'ng-zorro-antd/menu';
+import { NzFooterComponent } from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NzIconModule, NzMenuDirective, NzMenuDirective, NzFooterComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  protected readonly date = new Date();
+}
