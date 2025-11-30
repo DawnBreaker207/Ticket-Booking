25f4e52b651ec17b2a1c3fb9c7713e1037fc3982
DawnBreaker <tunganhngo207@gmail.com>
2025-11-30 21:39:30 +0700

    feat: update dashboard dto

--- backend/src/main/java/com/dawn/backend/dto/response/dashboard/PaymentDistributionDTO.java
+++ backend/src/main/java/com/dawn/backend/dto/response/dashboard/PaymentDistributionDTO.java
@@ -3,9 +3,11 @@ package com.dawn.backend.dto.response.dashboard;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class PaymentDistributionDTO {
     private String method;
--- backend/src/main/java/com/dawn/backend/dto/response/dashboard/TopMovieDTO.java
+++ backend/src/main/java/com/dawn/backend/dto/response/dashboard/TopMovieDTO.java
@@ -3,10 +3,12 @@ package com.dawn.backend.dto.response.dashboard;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TopMovieDTO {
     private String movieName;
     private Long ticketSold;
