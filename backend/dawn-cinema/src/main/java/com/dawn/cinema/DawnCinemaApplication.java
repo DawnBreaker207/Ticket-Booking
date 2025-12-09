1c1b1701b6f7f9a31da9e801901701fddedc60dd
DawnBreaker <tunganhngo207@gmail.com>
2025-12-10 01:51:50 +0700

    feat: update cinema module

---
+++ backend/dawn-cinema/src/main/java/com/dawn/cinema/DawnCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.dawn.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawnCinemaApplication.class, args);
+    }
+
+}
