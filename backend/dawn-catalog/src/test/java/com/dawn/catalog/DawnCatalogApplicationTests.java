c41c92e0f5fb9b1bea9710395cf7276f2aff2cd9
DawnBreaker <tunganhngo207@gmail.com>
2025-12-10 01:51:26 +0700

    feat: update catalog module

---
+++ backend/dawn-catalog/src/test/java/com/dawn/catalog/DawnCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawn.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
