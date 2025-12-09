f15b80510b454efcd81006ef3f6723dfb4132846
DawnBreaker <tunganhngo207@gmail.com>
2025-12-10 01:51:02 +0700

    feat: update identity module

---
+++ backend/dawn-identity/src/main/java/com/dawn/identity/DawnIdentityApplication.java
@@ -0,0 +1,13 @@
+package com.dawn.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnIdentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawnIdentityApplication.class, args);
+	}
+
+}
