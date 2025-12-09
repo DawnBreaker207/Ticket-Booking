d595355c2210e3ec07d40bfce9bdd01323bc66dc
DawnBreaker <tunganhngo207@gmail.com>
2025-12-10 01:52:39 +0700

    feat: update booking module

---
+++ backend/dawn-booking/src/main/java/com/dawn/booking/DawnBookingApplication.java
@@ -0,0 +1,13 @@
+package com.dawn.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawnBookingApplication.class, args);
+    }
+
+}
---
+++ backend/dawn-booking/src/test/java/com/dawn/booking/DawnBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawn.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
