1531555a8126e474a9f7e56b03c4bd473c34e178
DawnBreaker <tunganhngo207@gmail.com>
2025-12-10 14:22:59 +0700

    feat: update web module

---
+++ backend/dawn-web/src/main/java/com/dawn/web/DawnWebApplication.java
@@ -0,0 +1,13 @@
+package com.dawn.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawnWebApplication.class, args);
+    }
+
+}
---
+++ backend/dawn-web/src/test/java/com/dawn/web/DawnWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawn.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
