855117d35d78ff5f59160bda81c2f0ecedc7ffe3
DawnBreaker <tunganhngo207@gmail.com>
2025-12-11 14:49:56 +0700

    feat: update rabbitmq config

---
+++ backend/src/main/java/com/dawn/backend/config/RabbitMQConfig.java
@@ -0,0 +1,46 @@
+package com.dawn.backend.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String QUEUE_NOTIFY = "notification_queue";
+    public static final String EXCHANGE_NOTIFY = "notification_exchange";
+    public static final String ROUTING_KEY_NOTIFY = "notification_routing_key";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NOTIFY);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NOTIFY);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY_NOTIFY);
+    }
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+}
