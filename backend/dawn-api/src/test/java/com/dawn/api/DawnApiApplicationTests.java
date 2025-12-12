56d2e04a34131e5ee360dff95d07f04bc0b089cc
DawnBreaker <tunganhngo207@gmail.com>
2025-12-12 10:57:57 +0700

    feat: update api module

---
+++ backend/dawn-api/src/test/java/com/dawn/api/DawnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawn.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
