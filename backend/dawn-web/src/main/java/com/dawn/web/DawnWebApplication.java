f54a89e9f2428feb5c3af2defc268703d5f2e895
DawnBreaker <tunganhngo207@gmail.com>
2025-12-12 22:17:47 +0700

    feat: update web module

--- backend/dawn-web/src/main/java/com/dawn/web/DawnWebApplication.java
+++ backend/dawn-web/src/main/java/com/dawn/web/DawnWebApplication.java
@@ -2,11 +2,15 @@ package com.dawn.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.dawn")
+@EntityScan(basePackages = "com.dawn")
+@EnableJpaRepositories(basePackages = "com.dawn")
 @EnableCaching
 @EnableScheduling
 @EnableJpaAuditing
