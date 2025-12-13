f742d3030be2acfc1bf903547ba9b0518271466a
DawnBreaker <tunganhngo207@gmail.com>
2025-12-13 22:32:44 +0700

    feat: update web module

--- backend/dawn-web/src/main/java/com/dawn/web/config/WebConfig.java
+++ backend/dawn-web/src/main/java/com/dawn/web/config/WebConfig.java
@@ -3,8 +3,10 @@ package com.dawn.web.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
@@ -14,16 +16,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public RestTemplate restTemplate() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(10_000);
-        factory.setReadTimeout(10_000);
-        return new RestTemplate(factory);
-    }
-
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api/v1", HandlerTypePredicate.forAnnotation(RestController.class));
     }
+
+    @Bean
+    public RestClient restClient(RestClient.Builder builder) {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(10_000);
+        factory.setReadTimeout(10_000);
+
+        return builder
+                .requestFactory(factory)
+                .build();
+    }
+
 }
