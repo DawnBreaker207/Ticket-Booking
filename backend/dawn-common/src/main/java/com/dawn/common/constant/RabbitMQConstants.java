26f43748dfce6b901a381a5598ad9f12237c40f9
DawnBreaker <tunganhngo207@gmail.com>
2025-12-16 20:52:21 +0700

    feat: update common module

---
+++ backend/dawn-common/src/main/java/com/dawn/common/constant/RabbitMQConstants.java
@@ -0,0 +1,7 @@
+package com.dawn.common.constant;
+
+public class RabbitMQConstants {
+    public static final String QUEUE_NOTIFY = "notification_queue";
+    public static final String EXCHANGE_NOTIFY = "notification_exchange";
+    public static final String ROUTING_KEY_NOTIFY = "notification_routing_key";
+}
