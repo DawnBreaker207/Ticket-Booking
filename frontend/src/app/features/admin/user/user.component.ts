ecbcf306d6e0dda301fe3cf0bdbcc04761beaadb
DawnBreaker <tunganhngo207@gmail.com>
2025-12-21 23:54:20 +0700

    feat: update user table admin

--- frontend/src/app/features/admin/user/user.component.ts
+++ frontend/src/app/features/admin/user/user.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { NzTableModule } from 'ng-zorro-antd/table';
+import { Component, inject, OnInit } from '@angular/core';
+import { NzTableModule, NzTableQueryParams } from 'ng-zorro-antd/table';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
 import { NzIconModule } from 'ng-zorro-antd/icon';
@@ -8,10 +8,9 @@ import { DatePipe } from '@angular/common';
 import { NzTagComponent } from 'ng-zorro-antd/tag';
 import { FormsModule } from '@angular/forms';
 import { IsDeletedPipe } from '@core/pipes/is-deleted.pipe';
-import { UserService } from '@domain/user/data-access/user.service';
-import { User } from '@domain/user/models/user.model';
-import { Pagination } from '@core/models/common.model';
 import { headerColumns } from '@core/constants/column';
+import { LoadingComponent } from '@shared/components/loading/loading.component';
+import { UserStore } from '@domain/user/data-access/user.store';
 
 @Component({
   selector: 'app-user',
@@ -26,44 +25,34 @@ import { headerColumns } from '@core/constants/column';
     NzTagComponent,
     FormsModule,
     IsDeletedPipe,
+    LoadingComponent,
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent implements OnInit {
-  private userService = inject(UserService);
-  userList: readonly User[] = [];
-  pagination: Pagination | null = null;
+  readonly userStore = inject(UserStore);
+
   headerColumns = headerColumns.user;
-  value = true;
   pageIndex = 1;
   pageSize = 10;
-  isLoading = signal<boolean>(true);
 
   ngOnInit() {
     this.loadUsers();
   }
 
   loadUsers() {
-    this.userService.getAll({ page: 0, size: 10 }).subscribe({
-      next: (res) => {
-        if (res) {
-          this.isLoading.set(false);
-          this.userList = res.content;
-          this.pagination = res.pagination;
-        }
-      },
-      error: () => {
-        this.isLoading.set(true);
-      },
-    });
+    this.userStore.loadUsers({ page: this.pageIndex - 1, size: this.pageSize });
   }
 
   updateStatus(id: number, status: boolean) {
-    this.isLoading.set(true);
-    this.userService.updateStatus(id, status).subscribe(() => {
-      this.isLoading.set(false);
-      this.loadUsers();
-    });
+    this.userStore.updateUserStatus({ id: id, isDeleted: status });
+  }
+
+  onQueryParamsChange(params: NzTableQueryParams) {
+    const { pageIndex, pageSize } = params;
+    this.pageIndex = pageIndex;
+    this.pageSize = pageSize;
+    this.loadUsers();
   }
 }
