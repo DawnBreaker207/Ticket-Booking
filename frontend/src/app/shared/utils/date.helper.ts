a839b4deeb7ecee97af09967df2087f9e4a07342
DawnBreaker <tunganhngo207@gmail.com>
2025-12-23 16:52:52 +0700

    feat: update date helper

--- frontend/src/app/shared/utils/date.helper.ts
+++ frontend/src/app/shared/utils/date.helper.ts
@@ -15,3 +15,11 @@ export const timeFormat = (time: Date | string | null | undefined) => {
   if (!dateTime.isValid()) return '-';
   return dateTime.format('HH:mm');
 };
+
+export const timeFormatDay = (time: Date | string | null | undefined) => {
+  if (!time) return '-';
+  const today = dayjs().format('YYYY-MM-DD');
+  const dateTime = dayjs(`${today} ${time}`, 'YYYY-MM-DD HH:mm:ss');
+  if (!dateTime.isValid()) return '-';
+  return dateTime;
+};
